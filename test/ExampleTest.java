2c46d2fad0e6f99bf64ca166db98e4d264822bf8
Francisco Oliveira <fr.pdoliv@gmail.com>
2022-03-29 22:31:48 +0100

    More errors

--- test/ExampleTest.java
+++ test/ExampleTest.java
@@ -2,15 +2,25 @@ import org.junit.Test;
 
 import pt.up.fe.comp.TestUtils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ExampleTest {
 
     @Test
     public void testExpression() {
-        TestUtils.parse("2+3\n10+20\n");
-        // var parserResult = TestUtils.parse("2+3\n10+20\n");
-        // parserResult.getReports().get(0).getException().get().printStackTrace();
-        // // System.out.println();
-        // var analysisResult = TestUtils.analyse(parserResult);
+        //TestUtils.mustFail(parseResult.getReports());
+        try {
+            File myFile = new File("test/fixtures/public/FindMaximum.jmm");
+            String myReader = new Scanner(myFile).useDelimiter("\\Z").next();
+            System.out.println(myReader);
+            var parserResult = TestUtils.parse(myReader);
+            parserResult.getReports().get(0).getException().get().printStackTrace();
+            //var analysisResult = TestUtils.analyse(parserResult);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.toString());
+        }
     }
 
 }
